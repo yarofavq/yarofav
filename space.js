1f125a74c9c362c31b91c1b33323b4d9be5972ca
yarofavq <yarofavq69@gmail.com>
2026-09-19 17:43:12 +0300

    Update space.js

--- space.js
+++ space.js
@@ -2679,10 +2679,10 @@ window.addEventListener('resize', () => {
 // Clicker module loader (cache-busted)
 (function () {
   var s = document.createElement('script');
-  s.src = 'clicker.js?v=25';
+  s.src = 'clicker.js?v=36';
   document.body.appendChild(s);
   var e = document.createElement('script');
-  e.src = 'clicker_ext.js?v=25';
+  e.src = 'clicker_ext.js?v=36';
   document.body.appendChild(e);
 })();
 
