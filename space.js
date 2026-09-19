50c9bee86d0ddba79570329505939652770dd3e7
yarofavq <yarofavq69@gmail.com>
2026-09-19 20:31:43 +0300

    v47

--- space.js
+++ space.js
@@ -2679,10 +2679,10 @@ window.addEventListener('resize', () => {
 // Clicker module loader (cache-busted)
 (function () {
   var s = document.createElement('script');
-  s.src = 'clicker.js?v=47';
+  s.src = 'clicker.js?v=48';
   document.body.appendChild(s);
   var e = document.createElement('script');
-  e.src = 'clicker_ext.js?v=47';
+  e.src = 'clicker_ext.js?v=48';
   document.body.appendChild(e);
 })();
 
